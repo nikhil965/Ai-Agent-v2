277f618aa664b9529f624ba52215a8c7eeef98fd
Nikhil Kumar <nikhil965rajput@gmail.com>
2026-07-16 10:41:26 +0530

    helloo

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 BASE_URL = "https://api.groq.com/openai/v1"
 MODEL_NAME = "llama-3.3-70b-versatile"
 
-
 import os
 from dotenv import load_dotenv
-load_dotenv()
+
+result = load_dotenv()
+print("load_dotenv success:", result)
 
 API_KEY = os.getenv("GROQ_API_KEY")
+print("API_KEY loaded:", repr(API_KEY))
